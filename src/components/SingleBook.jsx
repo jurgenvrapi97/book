d68f846dd11f8cb2d6c4556b27e7d709c4bc7907
jurgenvrapi97 <jurgenvrapi@gmail.com>
2024-01-31 16:59:13 +0100

    colori bordo

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,16 +1,34 @@
 import { Card } from 'react-bootstrap'
+import React, { Component } from 'react'
 
-function CardComponent(props) {
-  return (
-    <Card className="mt-3">
-      <Card.Img style={{ height: '400px' }} variant="top" src={props.img} />
-      <Card.Body style={{ height: '200px' }}>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>Categoria: {props.category}</Card.Text>
-        <Card.Text>Prezzo: {props.price}</Card.Text>
-      </Card.Body>
-    </Card>
-  )
+class SingleBook extends Component {
+  state = {
+    clicked: false,
+  }
+
+  handleClick = () => {
+    console.log('funziona')
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+  render() {
+    const cardClass = this.state.clicked ? 'border border-danger' : ''
+
+    return (
+      <Card className={`mt-3 ${cardClass}`} onClick={this.handleClick}>
+        <Card.Img
+          style={{ height: '400px' }}
+          variant="top"
+          src={this.props.img}
+        />
+        <Card.Body style={{ height: '200px' }}>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>Categoria: {this.props.category}</Card.Text>
+          <Card.Text>Prezzo: {this.props.price}</Card.Text>
+        </Card.Body>
+      </Card>
+    )
+  }
 }
 
-export default CardComponent
+export default SingleBook
