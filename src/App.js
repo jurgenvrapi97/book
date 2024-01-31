0f1292430c3aed13fc4d96473231f77d55016001
jurgenvrapi97 <jurgenvrapi@gmail.com>
2024-01-31 16:28:25 +0100

    added input end singleBook component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBook from './components/NavComponent'
-import ContainerBook from './components/BookShopComponent'
+import BookList from './components/BookList'
 import AdditionalContentExample from './components/AllertComponent'
+import FooterComponent from './components/FooterComponent'
+
 function App() {
   return (
-    <div className="App">
-      <AdditionalContentExample />
-      <NavBook />
-      <ContainerBook />
-    </div>
+    <>
+      <div className="App">
+        <header>
+          {' '}
+          <AdditionalContentExample />
+          <NavBook />
+        </header>
+        <main>
+          <BookList />
+        </main>
+        <footer>
+          <FooterComponent />
+        </footer>
+      </div>
+    </>
   )
 }
 
